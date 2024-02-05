d22c56b2cddbb88a1f998ba8ce970959da8b8416
WilliamHR <s194545@dtu.dk>
2024-02-05 21:08:35 +0100

    testing fastp

--- src/02_fastp_trim.sh
+++ src/02_fastp_trim.sh
@@ -63,7 +63,7 @@ trim_command() {
         trim1=$(echo $base | sed "s/.fastq.gz//")_trimmed_1.fastq.gz
         trim2=$(echo $base | sed "s/.fastq.gz//")_trimmed_2.fastq.gz
         st=$(echo $base | sed "s/.fastq.gz//")_trimmed_singleton.fastq.gz
-        /home/ctools/fastp/fastp --in1=$1 --in2=$read2 --out1="$trimmed_dir"$trim1 --out2="$trimmed_dir"$trim2 --thread $threads -l $minlength --cut_tail --adapter_sequence=AATGATACGGCGACCACCGAGATCTACACGCT --adapter_sequence_r2=CAAGCAGAAGACGGCATACGAGAT
+        /home/ctools/fastp/fastp --in1=$1 --in2=$read2 --out1="$trimmed_dir"$trim1 --out2="$trimmed_dir"$trim2 --thread $thread -l $minlength --cut_tail --adapter_sequence=AATGATACGGCGACCACCGAGATCTACACGCT --adapter_sequence_r2=CAAGCAGAAGACGGCATACGAGAT
     else
         trim1=$(echo $base | sed "s/.fastq.gz//")_trimmed.fastq.gz
         /home/ctools/fastp/fastp in1=$1 out1="$trimmed_dir"$trim1 thread=$threads l=$minlength
