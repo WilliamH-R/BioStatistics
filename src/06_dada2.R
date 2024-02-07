4c3a947c92865c75bf3b48f3767899716a024eb4
WilliamHR <s194545@dtu.dk>
2024-02-07 10:00:28 +0100

    Ran bowtie2

--- src/06_dada2.R
+++ src/06_dada2.R
@@ -1,12 +1,10 @@
-# dada2
-
 library('tidyverse')
 library('dada2')
 library('here')
 library('phyloseq'); packageVersion("phyloseq")
 library('Biostrings'); packageVersion("Biostrings")
-library('ggplot2'); packageVersion("ggplot2")
-theme_set(theme_bw())
+#library('ggplot2'); packageVersion("ggplot2")
+#theme_set(theme_bw())
 
 # Set path to input trimmed files
 path <- here("data/bowtie2/")
