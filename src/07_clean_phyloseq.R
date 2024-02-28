35149fbf8353064e926fef4b30b9a8f5482c9c24
WilliamHR <s194545@dtu.dk>
2024-02-28 10:51:56 +0100

    Use native R pipe

--- src/07_clean_phyloseq.R
+++ src/07_clean_phyloseq.R
@@ -1,5 +1,4 @@
 # Packages
-library("magrittr")
 library("phyloseq")
 library("dplyr")
 library("tidyr")
@@ -8,29 +7,29 @@ library("tibble")
 
 
 # Load data object and select needed objects
-physeq_tbl <- readRDS("data/dada2/dada2.phyloseq.rds") %>%
-  psmelt() %>% 
+physeq_tbl <- readRDS("data/dada2/dada2.phyloseq.rds") |>
+  psmelt() |> 
   select(Sample, Abundance, Kingdom, Phylum,
          Class, Order, Family, Genus, Species)
 
 # Amalgamate to genus level, too many unknown species
-physeq_tbl_amal <- physeq_tbl %>%
+physeq_tbl_amal <- physeq_tbl |>
   group_by(Sample,
-           Genus) %>% 
+           Genus) |> 
   summarize(Abundance = sum(Abundance))
 
 # Drop Genus present in less than 10% of samples
-physeq_tbl_amal_filt <- physeq_tbl_amal %>% 
-  group_by(Genus) %>% 
-  add_count(name = "Count") %>%
+physeq_tbl_amal_filt <- physeq_tbl_amal |> 
+  group_by(Genus) |> 
+  add_count(name = "Count") |>
   mutate(Count_non_zero = sum(Abundance != 0),
-         Frac_non_zero = Count_non_zero / Count) %>%
-  ungroup() %>% 
-  filter(Frac_non_zero > 0.1) %>%
+         Frac_non_zero = Count_non_zero / Count) |>
+  ungroup() |> 
+  filter(Frac_non_zero > 0.1) |>
   select(Sample, Genus, Abundance)
 
 # Pivot wider which creates count matrix
-count_matrix <- physeq_tbl_amal_filt %>% 
+count_matrix <- physeq_tbl_amal_filt |> 
   pivot_wider(names_from = Genus,
               values_from = Abundance,
               values_fill = 0)
