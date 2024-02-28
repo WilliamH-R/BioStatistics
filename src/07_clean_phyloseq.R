d9ba719e6f85525eab8b786b692fd8a5456e6040
WilliamHR <s194545@dtu.dk>
2024-02-28 10:45:52 +0100

    Cleaned phyloseq object, created count matrix

--- src/07_clean_phyloseq.R
+++ src/07_clean_phyloseq.R
@@ -3,6 +3,7 @@ library("magrittr")
 library("phyloseq")
 library("dplyr")
 library("tidyr")
+library("tibble")
 
 
 
@@ -18,14 +19,25 @@ physeq_tbl_amal <- physeq_tbl %>%
            Genus) %>% 
   summarize(Abundance = sum(Abundance))
 
+# Drop Genus present in less than 10% of samples
+physeq_tbl_amal_filt <- physeq_tbl_amal %>% 
+  group_by(Genus) %>% 
+  add_count(name = "Count") %>%
+  mutate(Count_non_zero = sum(Abundance != 0),
+         Frac_non_zero = Count_non_zero / Count) %>%
+  ungroup() %>% 
+  filter(Frac_non_zero > 0.1) %>%
+  select(Sample, Genus, Abundance)
+
 # Pivot wider which creates count matrix
-physeq_tbl_amal %>% 
+count_matrix <- physeq_tbl_amal_filt %>% 
   pivot_wider(names_from = Genus,
               values_from = Abundance,
               values_fill = 0)
 
 
-
+saveRDS(object = count_matrix,
+        file = "data/count_matrix/count_matrix.rds")
 
 # OTU, Sample, Abundance, chem_administration, ETHNICITY, geo_loc_name,
 # Host_age, host_body_mass_index, Host_disease, host_phenotype, host_sex,
